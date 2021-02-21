72c0f1c441bf12777d6b4fcd51b5d198f324b6c6
verycumbersome <matthewsages@gmail.com>
2021-02-21 05:09:30 -0800

    Add forward pass

--- main.hs
+++ main.hs
@@ -17,41 +17,52 @@ m2 = [[1, 2, 3, 4],
 
 
 -- |Get the array of a matrix(m) at row(r)
-col :: [[Int]] -> Int -> [Int]
+col :: (Num a) => [[a]] -> Int -> [a]
 col m r = [l !! r | l <- m]
 
 -- |Get the array of a matrix(m) at col(r)
-row :: [[Int]] -> Int -> [Int]
+row :: (Num a) => [[a]] -> Int -> [a]
 row m c = m !! c
 
--- |Take the dot product of two Int arrays
-dot_product :: [Int] -> [Int] -> Int
+-- |Take the dot product of two vectors 
+dot_product :: (Num a) => [a] -> [a] -> a 
 dot_product [] [] = 0
 dot_product (x:xs) (y:ys) = (x * y) + dot_product xs ys
 
--- |Take the Haddamard product of two Int arrays
-haddamard :: [Int] -> [Int] -> [Int]
+-- |Take the Haddamard product of two vectors 
+haddamard :: (Num a) => [a] -> [a] -> [a] 
 haddamard [] [] = []
 haddamard (x:xs) (y:ys) = [x * y] ++ haddamard xs ys
 
 -- |Take the transpose of a matrix
-transpose :: [[Int]] -> [[Int]]
+transpose :: (Num a) => [[a]] -> [[a]]
 transpose m = [col m x | x <- [0..(length m) - 1]]
 
 -- |Multiply two matrices(x, y) of type Int
-matmul :: [[Int]] -> [[Int]] -> [[Int]]
+matmul :: (Num a) => [[a]] -> [[a]] -> [[a]]
 matmul _ [] = []
 matmul m1 (v2:m2) = [[dot_product v1 v2 | v1 <- m1]] ++ matmul m1 m2
 
 
 -- |Create a matrix of size (m, n) with random doubles from 0 to 1
-random_arr :: Int -> [Double]
-random_arr seed = randoms (mkStdGen seed) :: [Double]
+init_vec :: Int -> [Double]
+init_vec seed = randoms (mkStdGen seed) :: [Double]
 
 init_matrix :: Int -> Int -> [[Double]]
-init_matrix m n = [take n (random_arr 10) | _ <- [0..m]]
+init_matrix m n = [take n (init_vec s) | s <- [1..m]]
+
+
+-- Define nueral net
+x1 = [[1..4]]            --Initial input 
+w1 = init_matrix 50 4   --L1 weights(10, 4)
+b1 = init_matrix 50 1   --L1 biases(10, 1)
+
+w2 = init_matrix 10 50   --L1 weights(10, 4)
+b2 = init_matrix 10 1   --L1 biases(10, 1)
+
 
 main = do
-    init_matrix 4 5
+    let x2 = matmul w1 x1
+    let out = matmul w2 x2
 
-    print m1 
+    print(out)
