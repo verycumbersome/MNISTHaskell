b0838ab9956da898ceb275a31fa559f5d1038a1e
verycumbersome <matthewsages@gmail.com>
2021-02-21 02:14:00 -0800

    Add transpose

--- main.hs
+++ main.hs
@@ -10,14 +10,24 @@ m2 = [[1, 2, 3, 4],
     [1, 2, 3, 4],
     [1, 2, 3, 4]]
 
-row :: [[Int]] -> Int -> [Int]
-row m r = [l !! r | l <- m]
-
+-- |Get the array of a matrix(m) at row(r)
 col :: [[Int]] -> Int -> [Int]
-col m c = m !! c
+col m r = [l !! r | l <- m]
 
+-- |Get the array of a matrix(m) at col(r)
+row :: [[Int]] -> Int -> [Int]
+row m c = m !! c
+
+-- |Take the dot product of two Int arrays
 dot_product :: [Int] -> [Int] -> [Int]
 dot_product [] [] = []
 dot_product (x:xs) (y:ys) = [x * y] ++ dot_product xs ys
 
-matmul x y = head x + length y
+-- |Take the transpose of a matrix
+transpose :: [[Int]] -> [[Int]]
+transpose m = [col m x | x <- [0..(length m) - 1]]
+
+-- |Multiply two matrices(x, y) of type Int
+--matmul :: [[Int]] -> [[Int]] -> [[Int]]
+--matmul [] [] = 
+--matmul (x:xs) (y:ys) = 
