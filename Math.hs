bddf1d611bc3c21bdaf588118e25f54f45d302d4
verycumbersome <matthewsages@gmail.com>
2021-02-21 16:08:42 -0800

    Add inline vector operations

---
+++ Math.hs
@@ -0,0 +1,70 @@
+module Math where
+
+import System.Random
+newRand = randomIO :: IO Int
+
+m1 :: [[Int]]
+m1 = [[1, 2, 3, 4],
+    [1, 2, 3, 4],
+    [1, 2, 3, 4],
+    [1, 2, 3, 4]]
+
+m2 :: [[Int]]
+m2 = [[1, 2, 3, 4],
+    [1, 2, 3, 4],
+    [1, 2, 3, 4],
+    [1, 2, 3, 4]]
+
+
+-- |Get the array of a matrix(m) at row(r)
+col :: (Num a) => [[a]] -> Int -> [a]
+col m r = [l !! r | l <- m]
+
+-- |Get the array of a matrix(m) at col(r)
+row :: (Num a) => [[a]] -> Int -> [a]
+row m c = m !! c
+
+-- |Add two vectors
+v_add :: (Num a) => [a] -> [a] -> [a] 
+[] `v_add` [] = []
+(x:xs) `v_add` (y:ys) = [x + y] ++ xs `v_add` ys
+
+-- |Subtract two vectors
+v_sub :: (Num a) => [a] -> [a] -> [a] 
+[] `v_sub` [] = []
+(x:xs) `v_sub` (y:ys) = [x - y] ++ xs `v_sub` ys
+
+-- |Divide two vectors
+v_div :: [Int] -> [Int] -> [Int] 
+[] `v_div` [] = []
+(x:xs) `v_div` (y:ys) = [x `div` y] ++ xs `v_div` ys
+
+-- |Take the Haddamard product of two vectors 
+hadamard :: (Num a) => [a] -> [a] -> [a] 
+[] `hadamard` [] = []
+(x:xs) `hadamard` (y:ys) = [x * y] ++ xs `hadamard` ys
+
+-- |Take the dot product of two vectors 
+dot :: (Num a) => [a] -> [a] -> a 
+[] `dot` [] = 0
+(x:xs) `dot` (y:ys) = (x * y) + xs `dot` ys
+
+
+-- |Take the transpose of a matrix
+transpose :: (Num a) => [[a]] -> [[a]]
+transpose m = [col m x | x <- [0..(length m) - 1]]
+
+-- |Multiply two matrices(x, y) of type Int
+matmul :: (Num a) => [[a]] -> [[a]] -> [[a]]
+matmul _ [] = []
+matmul m1 (v2:m2) = [[v1 `dot` v2 | v1 <- m1]] ++ matmul m1 m2
+
+
+-- |Create a matrix of size (m, n) with random doubles from 0 to 1
+init_vec :: Int -> [Double]
+init_vec seed = randoms (mkStdGen seed) :: [Double]
+
+init_matrix :: Int -> Int -> [[Double]]
+init_matrix m n = [take n (init_vec s) | s <- [1..m]]
+
+
--- main.hs
+++ main.hs
@@ -1,56 +1,6 @@
 module Main where
 
-import System.Random
-newRand = randomIO :: IO Int
-
-m1 :: [[Int]]
-m1 = [[1, 2, 3, 4],
-    [1, 2, 3, 4],
-    [1, 2, 3, 4],
-    [1, 2, 3, 4]]
-
-m2 :: [[Int]]
-m2 = [[1, 2, 3, 4],
-    [1, 2, 3, 4],
-    [1, 2, 3, 4],
-    [1, 2, 3, 4]]
-
-
--- |Get the array of a matrix(m) at row(r)
-col :: (Num a) => [[a]] -> Int -> [a]
-col m r = [l !! r | l <- m]
-
--- |Get the array of a matrix(m) at col(r)
-row :: (Num a) => [[a]] -> Int -> [a]
-row m c = m !! c
-
--- |Take the dot product of two vectors 
-dot_product :: (Num a) => [a] -> [a] -> a 
-dot_product [] [] = 0
-dot_product (x:xs) (y:ys) = (x * y) + dot_product xs ys
-
--- |Take the Haddamard product of two vectors 
-haddamard :: (Num a) => [a] -> [a] -> [a] 
-haddamard [] [] = []
-haddamard (x:xs) (y:ys) = [x * y] ++ haddamard xs ys
-
--- |Take the transpose of a matrix
-transpose :: (Num a) => [[a]] -> [[a]]
-transpose m = [col m x | x <- [0..(length m) - 1]]
-
--- |Multiply two matrices(x, y) of type Int
-matmul :: (Num a) => [[a]] -> [[a]] -> [[a]]
-matmul _ [] = []
-matmul m1 (v2:m2) = [[dot_product v1 v2 | v1 <- m1]] ++ matmul m1 m2
-
-
--- |Create a matrix of size (m, n) with random doubles from 0 to 1
-init_vec :: Int -> [Double]
-init_vec seed = randoms (mkStdGen seed) :: [Double]
-
-init_matrix :: Int -> Int -> [[Double]]
-init_matrix m n = [take n (init_vec s) | s <- [1..m]]
-
+import Math
 
 -- Define nueral net
 x1 = [[1..4]]            --Initial input 
@@ -60,8 +10,17 @@ b1 = init_matrix 50 1   --L1 biases(10, 1)
 w2 = init_matrix 10 50   --L1 weights(10, 4)
 b2 = init_matrix 10 1   --L1 biases(10, 1)
 
+y = [[1..10]]
+
+v1 = [1..4]
+v2 = [1..4]
 
 main = do
+    print(v1 `v_add` v2)
+    print(v1 `v_sub` v2)
+    print(v1 `v_div` v2)
+    print(v1 `hadamard` v2)
+
     let x2 = matmul w1 x1
     let out = matmul w2 x2
 
