4f7a69f50f12378b127a7976608a04cfdedfdbf8
verycumbersome <matthewsages@gmail.com>
2021-02-22 10:33:01 -0800

    Update

--- Math.hs
+++ Math.hs
@@ -15,6 +15,8 @@ m2 = [[1, 2, 3, 4],
     [1, 2, 3, 4],
     [1, 2, 3, 4]]
 
+type Vec = [Double]
+--data Matrix a = Matrix [Vec a] deriving (Eq, Show)
 
 -- |Get the array of a matrix(m) at row(r)
 col :: (Num a) => [[a]] -> Int -> [a]
@@ -24,10 +26,29 @@ col m r = [l !! r | l <- m]
 row :: (Num a) => [[a]] -> Int -> [a]
 row m c = m !! c
 
+--infixr 5 .+
+--(.+) :: Num a => [a] -> [a] -> [a] 
+--[] .+ [] = []
+--(x:xs) (.+) (y:ys) = [x + y] ++ xs .+  ys
+
 -- |Add two vectors
-v_add :: (Num a) => [a] -> [a] -> [a] 
-[] `v_add` [] = []
-(x:xs) `v_add` (y:ys) = [x + y] ++ xs `v_add` ys
+infixr 5 .+
+(.+) :: (Num a) => [a] -> [a] -> [a] 
+[] .+ [] = []
+(x:xs) .+ (y:ys) = [x + y] ++ xs .+ ys
+
+--instance Num Matrix where 
+    --[] + [] = []
+    --(x:xs) + (y:ys) = [x + y] ++ xs + ys
+
+--v_add :: (Num a) => [a] -> [a] -> [a] 
+--[] `v_add` [] = []
+--(x:xs) `v_add` (y:ys) = [x + y] ++ xs `v_add` ys
+
+---- |Add two vectors
+--v_add :: (Num a) => [a] -> [a] -> [a] 
+--[] `v_add` [] = []
+--(x:xs) `v_add` (y:ys) = [x + y] ++ xs `v_add` ys
 
 -- |Subtract two vectors
 v_sub :: (Num a) => [a] -> [a] -> [a] 
--- main.hs
+++ main.hs
@@ -14,12 +14,15 @@ y = [[1..10]]
 
 v1 = [1..4]
 v2 = [1..4]
+--
+--mat = Matrix [[1..4]]
 
 main = do
-    print(v1 `v_add` v2)
-    print(v1 `v_sub` v2)
-    print(v1 `v_div` v2)
-    print(v1 `hadamard` v2)
+    --print(v1 `v_add` v2)
+    --print(v1 + v2)
+    --print(v1 `v_sub` v2)
+    --print(v1 `v_div` v2)
+    --print(v1 `hadamard` v2)
 
     let x2 = matmul w1 x1
     let out = matmul w2 x2
