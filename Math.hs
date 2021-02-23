b14520e072ae06e512e6305324ed93fec8358fe7
verycumbersome <matthewsages@gmail.com>
2021-02-22 21:02:38 -0800

    Add Tensor class

--- Math.hs
+++ Math.hs
@@ -1,64 +1,54 @@
 module Math where
 
+
 import System.Random
 newRand = randomIO :: IO Int
 
-m1 :: [[Int]]
-m1 = [[1, 2, 3, 4],
-    [1, 2, 3, 4],
-    [1, 2, 3, 4],
-    [1, 2, 3, 4]]
+m1 = Matrix [[1, 2, 3, 4],
+            [1, 2, 3, 4],
+            [1, 2, 3, 4],
+            [1, 2, 3, 4]]
 
-m2 :: [[Int]]
-m2 = [[1, 2, 3, 4],
-    [1, 2, 3, 4],
-    [1, 2, 3, 4],
-    [1, 2, 3, 4]]
+m2 = Matrix [[1, 2, 3, 4],
+            [1, 2, 3, 4],
+            [1, 2, 3, 4],
+            [1, 2, 3, 4]]
 
-type Vec = [Double]
---data Matrix a = Matrix [Vec a] deriving (Eq, Show)
 
--- |Get the array of a matrix(m) at row(r)
-col :: (Num a) => [[a]] -> Int -> [a]
-col m r = [l !! r | l <- m]
+data Tensor a = Vec a | Matrix a | Tensor [Tensor a] deriving (Eq, Show)
 
--- |Get the array of a matrix(m) at col(r)
-row :: (Num a) => [[a]] -> Int -> [a]
-row m c = m !! c
 
+-- VECTOR OPERATIONS --
+-- |Change fixity
 --infixr 5 .+
---(.+) :: Num a => [a] -> [a] -> [a] 
---[] .+ [] = []
---(x:xs) (.+) (y:ys) = [x + y] ++ xs .+  ys
+infixr 5 .-
+infixr 6 ./
+infixr 6 `hadamard`
+
+--instance Show Tensor where
+    --show Vec = "Vec"
+    --show Matrix = "Matrix"
+
+
+stringify :: Tensor a -> [a] -> [a]
+stringify _ [] = []
+stringify t (x:xs) = [x] ++ stringify t xs
+
 
 -- |Add two vectors
-infixr 5 .+
 (.+) :: (Num a) => [a] -> [a] -> [a] 
 [] .+ [] = []
 (x:xs) .+ (y:ys) = [x + y] ++ xs .+ ys
 
---instance Num Matrix where 
-    --[] + [] = []
-    --(x:xs) + (y:ys) = [x + y] ++ xs + ys
-
---v_add :: (Num a) => [a] -> [a] -> [a] 
---[] `v_add` [] = []
---(x:xs) `v_add` (y:ys) = [x + y] ++ xs `v_add` ys
-
----- |Add two vectors
---v_add :: (Num a) => [a] -> [a] -> [a] 
---[] `v_add` [] = []
---(x:xs) `v_add` (y:ys) = [x + y] ++ xs `v_add` ys
-
 -- |Subtract two vectors
-v_sub :: (Num a) => [a] -> [a] -> [a] 
-[] `v_sub` [] = []
-(x:xs) `v_sub` (y:ys) = [x - y] ++ xs `v_sub` ys
+(.-) :: (Num a) => [a] -> [a] -> [a] 
+[] .- [] = []
+(x:xs) .- (y:ys) = [x - y] ++ xs .- ys
 
 -- |Divide two vectors
-v_div :: [Int] -> [Int] -> [Int] 
-[] `v_div` [] = []
-(x:xs) `v_div` (y:ys) = [x `div` y] ++ xs `v_div` ys
+(./) :: [Int] -> [Int] -> [Int] 
+[] ./ [] = []
+(x:xs) ./ (y:ys) = [x `div` y] ++ xs ./ ys
 
 -- |Take the Haddamard product of two vectors 
 hadamard :: (Num a) => [a] -> [a] -> [a] 
@@ -70,6 +60,19 @@ dot :: (Num a) => [a] -> [a] -> a
 [] `dot` [] = 0
 (x:xs) `dot` (y:ys) = (x * y) + xs `dot` ys
 
+-- |Create a matrix of size (m, n) with random doubles from 0 to 1
+init_vec :: Int -> [Double]
+init_vec seed = randoms (mkStdGen seed) :: [Double]
+
+
+-- MATRIX OPERATIONS --
+-- |Get the array of a matrix(m) at row(r)
+col :: (Num a) => [[a]] -> Int -> [a]
+col m r = [l !! r | l <- m]
+
+-- |Get the array of a matrix(m) at col(r)
+row :: (Num a) => [[a]] -> Int -> [a]
+row m c = m !! c
 
 -- |Take the transpose of a matrix
 transpose :: (Num a) => [[a]] -> [[a]]
@@ -79,11 +82,6 @@ transpose m = [col m x | x <- [0..(length m) - 1]]
 matmul :: (Num a) => [[a]] -> [[a]] -> [[a]]
 matmul _ [] = []
 matmul m1 (v2:m2) = [[v1 `dot` v2 | v1 <- m1]] ++ matmul m1 m2
-
-
--- |Create a matrix of size (m, n) with random doubles from 0 to 1
-init_vec :: Int -> [Double]
-init_vec seed = randoms (mkStdGen seed) :: [Double]
 
 init_matrix :: Int -> Int -> [[Double]]
 init_matrix m n = [take n (init_vec s) | s <- [1..m]]
--- main.hs
+++ main.hs
@@ -12,19 +12,27 @@ b2 = init_matrix 10 1   --L1 biases(10, 1)
 
 y = [[1..10]]
 
-v1 = [1..4]
-v2 = [1..4]
---
---mat = Matrix [[1..4]]
+v1 = Vec [1..4]
+v2 = Vec [1..4]
+
+
+--layer :: (Num a) => [a] -> [[a]] -> [[a]]
+--layer W x b = W  x .+ b
+
+mat = Matrix [v1,
+              v1,
+              v1,
+              v1]
 
 main = do
-    --print(v1 `v_add` v2)
-    --print(v1 + v2)
-    --print(v1 `v_sub` v2)
-    --print(v1 `v_div` v2)
+    --stringify mat
+    print("Hello")
+    --print(v1 .+ v2)
+    --print(v1 .- v2)
+    --print(v1 ./ v2)
     --print(v1 `hadamard` v2)
 
-    let x2 = matmul w1 x1
-    let out = matmul w2 x2
+    --let x2 = matmul w1 x1
+    --let out = matmul w2 x2
 
-    print(out)
+    --print(out)
