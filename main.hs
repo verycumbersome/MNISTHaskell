97b191c256b3a665c767f4f97bc62379b7c0eb21
verycumbersome <matthewsages@gmail.com>
2021-02-23 21:19:06 -0800

    Add softmax

--- main.hs
+++ main.hs
@@ -2,8 +2,7 @@ module Main where
 
 import Math
 
-euler = exp 1
-
+euler = exp 1 
 
 -- Define nueral net
 x1 = [[1..4]]            --Initial input 
@@ -24,12 +23,23 @@ layer x w = matmul w x
 
 -- |Sigmoid function for a matrix
 sigmoid :: [[Double]] -> [[Double]]
-sigmoid x = [[(1 / (1 + euler ** (-i))) | i <- j] | j <- x]
+sigmoid x = [[(1 / (1 + exp (-i))) | i <- j] | j <- x]
+
+-- |Softmax function for a matrix
+softmax :: [[Double]] -> [[Double]]
+softmax x = [[(exp z) / sum (map (exp) y) | z <- y] | y <- x]
+
+
+-- |TODO
+sub :: Num a => [[a]] -> a -> [[a]]
+sub x y = [[i - y | i <- j] | j <- x]
+
+mul :: Num a => [[a]] -> a -> [[a]]
+mul x y = [[i * y | i <- j] | j <- x]
 
 main = do
-    --stringify mat
+    let  x2 = softmax (sigmoid [col (layer w1 x1) 0]) -- Col layer transposes matrix upright
+    let  out = softmax (sigmoid [col (layer w2 x2) 0])
 
-    let x2 = sigmoid [col (layer w1 x1) 0] -- Col layer transposes matrix upright
-    let out = sigmoid (layer w2 x2)
-
+    print(x2)
     print(out)
